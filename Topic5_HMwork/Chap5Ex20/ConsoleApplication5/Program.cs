7d71a46e455517fb4577d2b1589b691d6ce20ae0
REvansIII <hairbyrobbie@yahoo.com>
2016-12-19 02:10:46 -0700

    Topic5 Homework

---
+++ Topic5_HMwork/Chap5Ex20/ConsoleApplication5/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap5Ex20
+{
+    class Warehouse {
+
+      static ArrayList inventory = new ArrayList();
+
+        private String tv = "SuperScreen 1000";
+        private String radio = "Sound 1000";
+        private String computer = "Compute 1000";
+
+        public Warehouse() { }
+
+        public Warehouse(int t, int r, int c)
+        {
+            while (t > 0)
+            {
+                addTV();
+                t--;
+            }
+            while (r > 0)
+            {
+                addRadio();
+                r--;
+            }
+            while (c > 0)
+            {
+                addComputer();
+                c--;
+            }
+
+        }
+
+        public void addTV() {
+            inventory.Add(tv);
+
+        }
+
+        public void addRadio() {
+            inventory.Add(radio);         
+
+
+        }
+
+        public void addComputer() {
+            inventory.Add(computer);
+        }
+
+        public override string ToString()
+        {
+            return "Available appliances " + inventory;
+                
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Warehouse wh1 = new Warehouse();
+            wh1.addTV();
+            wh1.addRadio();
+            wh1.addComputer();
+            wh1.addTV();
+
+            Warehouse wh2 = new Warehouse(4, 2, 1);
+
+            Console.WriteLine("The appliances available are:");
+
+            foreach (string str in inventory)
+
+            {
+
+                Console.WriteLine(str);
+
+            }
+
+
+        }
+
+       
+    }
+}
---
+++ Topic5_HMwork/Chap5Ex22/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap5Ex22
+{
+    class Vending {
+        private double cupsAvailable;
+        private double cost;
+        private double payment;
+
+        Vending() {
+            cupsAvailable = 10;
+            cost = .50;
+        }
+
+        Vending(double cupsAvailable, double cost){
+            this.cupsAvailable = cupsAvailable;
+            this.cost = cost;
+
+
+        }
+
+        public void Menu() {
+            Console.WriteLine("The cost of a cup of coffee is {0:c} and there are {1} cups available", cost, cupsAvailable);
+
+        }
+
+        public void Insert(double quarter, double dime, double nickel) {
+            quarter = quarter *.25;
+            dime = dime *.10;
+            nickel = nickel *.05;
+
+            payment = quarter + dime + nickel;
+
+            Console.WriteLine("You have entered {0:c}", payment);
+
+        }
+
+        public void Select() {
+            if ((cupsAvailable >= 1))
+            {
+                cupsAvailable = payment / cost;
+                Console.WriteLine("You have purchased {0} cups of coffee at {1:c} a cup", cupsAvailable, cost);
+                
+            }
+            else if (cupsAvailable < 0)
+            {
+                Console.WriteLine("Coffee sold out");
+
+            }
+            else if (payment < cost)
+            {
+                Console.WriteLine("Funds are insufficient for this transaction");
+
+            }
+            else
+                Console.WriteLine("Out of order");            
+
+
+        }
+
+        public void Refund() {
+            Console.WriteLine("You have been refunded {0:c}", payment);
+        }
+    
+        static void Main(string[] args)
+        {
+            Vending vend1 = new Vending();
+            vend1.Menu();
+            vend1.Insert(3, 2, 1);
+            vend1.Select();
+
+            Vending vend2 = new Vending(2 , .75);
+            vend2.Menu();
+            vend2.Insert(4, 4, 2);
+            vend2.Select();
+
+            Vending vend3 = new Vending(0, .50);
+            vend3.Menu();
+            vend3.Insert(4, 0, 0);
+            vend3.Select();
+            vend3.Refund();
+
+
+
+        }
+    }
+}
